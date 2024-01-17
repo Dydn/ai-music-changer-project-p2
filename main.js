5828015bd59f1b4322b469e618adfadb8d63e2cd
DyDn <126629784+Dydn@users.noreply.github.com>
2024-01-16 16:59:35 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+song = "";
+
+function preload()
+{
+    song1 = loadSound("music.mp3")
+    song1 = loadSound("music.mp3")
+}
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
+
+function play()
+{
+    song.play();
+}
